b4ece36f6e71a6c4b2886eb099e5178eb7e3a006
王涛 <wangtao101rs@163.com>
2016-07-19 15:50:04 +0800

    青蛙跳台阶(斐波纳挈,简单动归)

---
+++ others/fib.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+
+"""
+一只青蛙一次可以跳上1级台阶，也可以跳上2级。求该青蛙跳上一个n级的台阶总共有多少种跳法。
+"""
+
+# 方法一
+fibn = lambda n: n if n <= 2 else fibn(n-1) + fibn(n-2)
+
+
+# 方法二
+def memo(func):
+    cache = {}
+
+    def warp(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return warp
+
+
+# 方法三
+def fib(n):
+    a, b = 0, 1
+    for _ in xrange(n):
+        a, b = b, a+b
+    return fib

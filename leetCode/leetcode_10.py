d1becb9c50c122d1f5974563a9e965b6cac93ef8
王涛 <wangtao101rs@163.com>
2017-01-04 14:44:19 +0800

    leetcode 10

---
+++ leetCode/leetcode_10.py
@@ -0,0 +1,61 @@
+import re
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        return re.match(p + '$', s) is not None
+
+
+class Solution2(object):
+    cache = {}
+
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if (s, p) in self.cache:
+            return self.cache[(s, p)]
+        if not p:
+            return not s
+        if p[-1] == '*':
+            if self.isMatch(s, p[:-2]):
+                self.cache[(s, p)] = True
+                return True
+            if s and (s[-1] == p[-2] or p[-2] == '.') and self.isMatch(s[:-1], p):
+                self.cache[(s, p)] = True
+                return True
+        if s and (p[-1] == s[-1] or p[-1] == '.') and self.isMatch(s[:-1], p[:-1]):
+            self.cache[(s, p)] = True
+            return True
+        self.cache[(s, p)] = False
+        return False
+
+
+class Solution3(object):
+
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False] * (len(s) + 1) for _ in range(len(p) + 1)]
+        dp[0][0] = True
+        for i in range(1, len(p)):
+            dp[i + 1][0] = dp[i - 1][0] and p[i] == '*'
+        for i in range(len(p)):
+            for j in range(len(s)):
+                if p[i] == '*':
+                    dp[i + 1][j + 1] = dp[i - 1][j + 1] or dp[i][j + 1]
+                    if p[i - 1] == s[j] or p[i - 1] == '.':
+                        dp[i + 1][j + 1] |= dp[i + 1][j]
+                else:
+                    dp[i + 1][j + 1] = dp[i][j] and (p[i] == s[j] or p[i] == '.')
+        return dp[-1][-1]

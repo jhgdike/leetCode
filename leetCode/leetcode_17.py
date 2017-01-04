959019dd63c529bf6909a9cb52d349e1f19abfda
王涛 <wangtao101rs@163.com>
2017-01-04 19:43:12 +0800

    leetcode 17

---
+++ leetCode/leetcode_17.py
@@ -0,0 +1,21 @@
+from itertools import product
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits:
+            return []
+        dic = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz',
+        }
+        arr = [dic[k] for k in digits]
+        return [''.join(x) for x in product(*arr)]

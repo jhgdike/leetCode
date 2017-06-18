a7d27d1c5aa077741e8b65255368cdcacfc9cd02
王涛 <wangtao101rs@163.com>
2017-06-19 01:50:58 +0800

    73

---
+++ Python/leetcode_72.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n, m = len(word1), len(word2)
+        dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
+        for i in range(n + 1):
+            dp[i][0] = i
+        for j in range(m + 1):
+            dp[0][j] = j
+        for i in range(n):
+            for j in range(m):
+                if word1[i] == word2[j]:
+                    dp[i + 1][j + 1] = dp[i][j]
+                else:
+                    dp[i + 1][j + 1] = 1 + min(
+                        (dp[i][j + 1], dp[i + 1][j], dp[i][j]))
+
+        return dp[-1][-1]

49729704cf70e7c7e2042dd1e50822647dbf06b5
王涛 <wangtao101rs@163.com>
2017-06-26 22:31:53 +0800

    300

---
+++ leetcode_python/300.py
@@ -0,0 +1,37 @@
+class Solution1(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        size = len(nums)
+        dp = []
+        for x in range(size):
+            low, high = 0, len(dp) - 1
+            while low <= high:
+                mid = (low + high) / 2
+                if dp[mid] >= nums[x]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            if low >= len(dp):
+                dp.append(nums[x])
+            else:
+                dp[low] = nums[x]
+        return len(dp)
+
+    def lengthOfLIS2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [1] * len(nums)
+        res = 0
+        for i in range(len(nums)):
+            max_val = 0
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    max_val = max(dp[j], max_val)
+            dp[i] += max_val
+            res = max(res, dp[i])
+        return res

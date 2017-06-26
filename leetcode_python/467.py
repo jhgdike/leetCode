1f6d19f121186d9ce77a4ce2e380f0d12e4108b0
王涛 <wangtao101rs@163.com>
2017-06-26 12:04:44 +0800

    467

---
+++ leetcode_python/467.py
@@ -0,0 +1,21 @@
+import collections
+
+
+class Solution(object):
+    def findSubstringInWraproundString(self, p):
+        """
+        :type p: str
+        :rtype: int
+        """
+        pattern = 'zabcdefghijklmnopqrstuvwxyz'
+        cmap = collections.defaultdict(int)
+        start = end = 0
+        for c in range(len(p)):
+            if c and p[c-1:c+1] not in pattern:
+                for x in range(start, end):
+                    cmap[p[x]] = max(end - x, cmap[p[x]])
+                start = c
+            end = c + 1
+        for x in range(start, end):
+            cmap[p[x]] = max(end - x, cmap[p[x]])
+        return sum(cmap.values())

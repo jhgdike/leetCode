3fccd36dcf4613dbeb4a3e3ce896b3da1aff3613
王涛 <wangtao101rs@163.com>
2017-06-27 09:56:02 +0800

    55

---
+++ leetcode_python/55.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        far = 0
+        for i in range(len(nums)):
+            if far >= len(nums) - 1:
+                return True
+            if far < i or far == i and nums[i] == 0:
+                return False
+            far = max(far, i + nums[i])
+
+print(Solution().canJump([3,2,1,0,4]))

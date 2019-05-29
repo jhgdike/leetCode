3e3d03d2d6f10b47165151b5ce98ab33e28ce1a5
wangtao <wangtao.92@bytedance.com>
2019-05-29 10:52:13 +0800

    15调试
    Change-Id: I74ae0f7f28d3a08c9063afa16a513188e3abb8d2

--- leetcode_python/1-100/15.py
+++ leetcode_python/1-100/15.py
@@ -23,3 +23,32 @@ class Solution(object):
                     else:
                         hi -= 1
         return res
+
+
+def tree_sum(nums):
+    nums.sort()
+    res = []
+    for i in range(len(nums)-2):
+        if nums[i] > 0:
+            break
+        if i == 0 or nums[i] > nums[i-1]:
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    res.append((nums[i], nums[l], nums[r]))
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+                elif s < 0:
+                    l += 1
+                else:
+                    r -= 1
+    return res
+
+
+tree_sum([-1,0,1,2,-1,-4])
